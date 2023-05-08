0c52ae522bbe1d095132a3674e88db37f83e7601
Sybatron <vipsister2014@gmail.com>
2023-05-08 12:19:22 +0300

    Updated my project setup

--- fusion/src/components/test.ts
+++
@@ -1,5 +0,0 @@
-const asd: [string, object] = ["asd", { banana: "isAwesome" }];
-type SecondTypeFromArray = (typeof asd)[1];
-//   ^?
-type TypeOfAnyArray = (typeof asd)[number];
-//   ^?
--- fusion/src/env.ts
+++ fusion/src/env.ts
@@ -1,5 +1,5 @@
-import { ZodError, z } from "zod";
 import { createEnv } from "@t3-oss/env-core";
+import { ZodError, z } from "zod";
 
 export const env = createEnv({
 	clientPrefix: "VITE_",
