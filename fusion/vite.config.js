97013cd419d30de965c3e0da5dc73efaa176faa2
Sybatron <vipsister2014@gmail.com>
2023-05-14 17:34:55 +0300

    Updateing config files

--- fusion/vite.config.js
+++ fusion/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const { PORT = 3001 } = process.env;
+const PORT = 3001;
 
 // https://vitejs.dev/config/
 export default defineConfig({
